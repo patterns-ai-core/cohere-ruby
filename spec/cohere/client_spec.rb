c522de7c8c4e52576a5c0ef034bec5c302ebcf4b
Andrei Bondarev <andrei.bondarev13@gmail.com>
2023-04-24 12:55:55 -0500

    standardrb fixes

--- spec/cohere/client_spec.rb
+++ spec/cohere/client_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Cohere::Client do
     it "returns a response" do
       expect(instance.generate(
         prompt: "Once upon a time in a magical land called"
-      ).dig('generations').first.dig('text')).to eq(" The Past there was a Game called Warhammer Fantasy Battle.")
+      ).dig("generations").first.dig("text")).to eq(" The Past there was a Game called Warhammer Fantasy Battle.")
     end
   end
 
@@ -35,7 +35,7 @@ RSpec.describe Cohere::Client do
     it "returns a response" do
       expect(instance.embed(
         texts: ["hello!"]
-      ).dig('embeddings')).to eq([[ 1.2177734, 0.67529297, 2.0742188 ]])
+      ).dig("embeddings")).to eq([[1.2177734, 0.67529297, 2.0742188]])
     end
   end
 
@@ -43,15 +43,19 @@ RSpec.describe Cohere::Client do
     let(:classify_result) { JSON.parse(File.read("spec/fixtures/classify_result.json")) }
     let(:response) { OpenStruct.new(body: classify_result) }
 
-    let(:inputs) {[
-      { text: "Dermatologists don't like her!", label: "Spam" },
-      { text: "Hello, open to this?", label: "Spam" }
-    ]}
-      
-    let(:examples) {[
-      "Confirm your email address",
-      "hey i need u to send some $"
-    ]}
+    let(:inputs) {
+      [
+        {text: "Dermatologists don't like her!", label: "Spam"},
+        {text: "Hello, open to this?", label: "Spam"}
+      ]
+    }
+
+    let(:examples) {
+      [
+        "Confirm your email address",
+        "hey i need u to send some $"
+      ]
+    }
 
     before do
       allow_any_instance_of(Faraday::Connection).to receive(:post)
@@ -63,10 +67,10 @@ RSpec.describe Cohere::Client do
       res = instance.classify(
         inputs: inputs,
         examples: examples
-      ).dig('classifications')
+      ).dig("classifications")
 
-      expect(res.first.dig('prediction')).to eq("Not spam")
-      expect(res.last.dig('prediction')).to eq("Spam")
+      expect(res.first.dig("prediction")).to eq("Not spam")
+      expect(res.last.dig("prediction")).to eq("Spam")
     end
   end
 
@@ -83,7 +87,7 @@ RSpec.describe Cohere::Client do
     it "returns a response" do
       expect(instance.tokenize(
         text: "Hello, world!"
-      ).dig('tokens')).to eq([33555, 1114 , 34])
+      ).dig("tokens")).to eq([33555, 1114, 34])
     end
   end
 
@@ -99,8 +103,8 @@ RSpec.describe Cohere::Client do
 
     it "returns a response" do
       expect(instance.detokenize(
-        tokens: [33555, 1114 , 34]
-      ).dig('text')).to eq("hello world!")
+        tokens: [33555, 1114, 34]
+      ).dig("text")).to eq("hello world!")
     end
   end
 
@@ -117,7 +121,7 @@ RSpec.describe Cohere::Client do
     it "returns a response" do
       expect(instance.detect_language(
         texts: ["Здравствуй, Мир"]
-      ).dig('results').first.dig('language_code')).to eq("ru")
+      ).dig("results").first.dig("language_code")).to eq("ru")
     end
   end
 
@@ -133,27 +137,27 @@ RSpec.describe Cohere::Client do
 
     it "returns a response" do
       expect(instance.summarize(
-        text: "Ice cream is a sweetened frozen food typically eaten as a snack or dessert. " +
-              "It may be made from milk or cream and is flavoured with a sweetener, " +
-              "either sugar or an alternative, and a spice, such as cocoa or vanilla, " +
-              "or with fruit such as strawberries or peaches. " +
-              "It can also be made by whisking a flavored cream base and liquid nitrogen together. " +
-              "Food coloring is sometimes added, in addition to stabilizers. " +
-              "The mixture is cooled below the freezing point of water and stirred to incorporate air spaces " +
-              "and to prevent detectable ice crystals from forming. The result is a smooth, " +
-              "semi-solid foam that is solid at very low temperatures (below 2 °C or 35 °F). " +
-              "It becomes more malleable as its temperature increases.\n\n" +
-              "The meaning of the name \"ice cream\" varies from one country to another. " +
-              "In some countries, such as the United States, \"ice cream\" applies only to a specific variety, " +
-              "and most governments regulate the commercial use of the various terms according to the " +
-              "relative quantities of the main ingredients, notably the amount of cream. " +
-              "Products that do not meet the criteria to be called ice cream are sometimes labelled " +
-              "\"frozen dairy dessert\" instead. In other countries, such as Italy and Argentina, " +
-              "one word is used fo\r all variants. Analogues made from dairy alternatives, " +
-              "such as goat's or sheep's milk, or milk substitutes " +
-              "(e.g., soy, cashew, coconut, almond milk or tofu), are available for those who are " +
+        text: "Ice cream is a sweetened frozen food typically eaten as a snack or dessert. " \
+              "It may be made from milk or cream and is flavoured with a sweetener, " \
+              "either sugar or an alternative, and a spice, such as cocoa or vanilla, " \
+              "or with fruit such as strawberries or peaches. " \
+              "It can also be made by whisking a flavored cream base and liquid nitrogen together. " \
+              "Food coloring is sometimes added, in addition to stabilizers. " \
+              "The mixture is cooled below the freezing point of water and stirred to incorporate air spaces " \
+              "and to prevent detectable ice crystals from forming. The result is a smooth, " \
+              "semi-solid foam that is solid at very low temperatures (below 2 °C or 35 °F). " \
+              "It becomes more malleable as its temperature increases.\n\n" \
+              "The meaning of the name \"ice cream\" varies from one country to another. " \
+              "In some countries, such as the United States, \"ice cream\" applies only to a specific variety, " \
+              "and most governments regulate the commercial use of the various terms according to the " \
+              "relative quantities of the main ingredients, notably the amount of cream. " \
+              "Products that do not meet the criteria to be called ice cream are sometimes labelled " \
+              "\"frozen dairy dessert\" instead. In other countries, such as Italy and Argentina, " \
+              "one word is used fo\r all variants. Analogues made from dairy alternatives, " \
+              "such as goat's or sheep's milk, or milk substitutes " \
+              "(e.g., soy, cashew, coconut, almond milk or tofu), are available for those who are " \
               "lactose intolerant, allergic to dairy protein or vegan."
-      ).dig('summary')).to eq("Ice cream is a frozen dessert made from dairy products or non-dairy substitutes. It is flavoured with a sweetener and a spice or with fruit. It is smooth and semi-solid at low temperatures.")
+      ).dig("summary")).to eq("Ice cream is a frozen dessert made from dairy products or non-dairy substitutes. It is flavoured with a sweetener and a spice or with fruit. It is smooth and semi-solid at low temperatures.")
     end
   end
 end
